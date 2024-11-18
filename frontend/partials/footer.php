dc59b4eed28b20c4833f6aa22cd847ef0b82a20c
IThuydg <huydgps39705@gmail.com>
2024-11-18 14:41:59 +0700

    initial commit

---
+++ frontend/partials/footer.php
@@ -0,0 +1,15 @@
+<footer class="footer mt-auto py-3 bg-dark text-white">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-2"></div>
+            <div class="col-md-10">
+                <span>Website nằm trong đồ án môn Kỹ năng nghề <br />
+                    Ngày báo cáo 11/09/2022<br />
+                    Nguồn tham khảo: https://truyentranh1.com/</span>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<!-- floating-action -->
+<?php require_once __DIR__.'/../partials/floating-action.php'?>
